13a5db8f7c2e30a3a1f19127678fcc0c5bd17729
rend1027 <rend1027@yahoo.com>
2025-06-30 13:43:08 -0400

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
   async function fetchAllTasks() {
     try {
-      const response = await axios.get("https://client-side-routing-frontend-seven.vercel.app/api/tasks");
+      const response = await axios.get("http://localhost:8080/api/tasks");
       setTasks(response.data);
     } catch (error) {
       console.error("Error fetching tasks:", error);
